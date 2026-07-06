ad42d252d97fe6672f596d6f9b2cca75cbb8f3d1
김동욱 <ehddnr73@naver.com>
2026-07-06 16:21:33 +0900

    프로젝트 생성

---
+++ src/main/java/yeolmok/springcachewithcaffeine/SpringCacheWithCaffeineApplication.java
@@ -0,0 +1,13 @@
+package yeolmok.springcachewithcaffeine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheWithCaffeineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCacheWithCaffeineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yeolmok/springcachewithcaffeine/SpringCacheWithCaffeineApplicationTests.java
@@ -0,0 +1,13 @@
+package yeolmok.springcachewithcaffeine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCacheWithCaffeineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
